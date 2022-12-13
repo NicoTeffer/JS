e8b3bb3af6aecde8f92ad8ce71ad5c9d4a75e0bd
Nico Teffer <n.teffer@gmail.com>
2022-12-13 21:44:11 +0100

    JSON and Object Literals

--- Code/app.js
+++ Code/app.js
@@ -1,27 +1,14 @@
-var greet = "Hello!";
-var greet = "Hola!";
-
-console.log(greet);
-
-var english = {};
-var spanish = {};
-
-english.greet = "Hello!";
-spanish.greet = "Hola!";
-
-console.log(spanish);
-console.log(english);
-
-english.greetings = {};
-
-english.greetings.greet = "Hi!";
-
-console.log(english.greetings);
-
-var english = {
-  greetings: {
-    basic: "Sup!",
-  },
+var objectLiteral = {
+  firstname: "Mary",
+  isAProgrammer: true,
 };
 
-console.log(english.greetings);
+console.log(
+    JSON.stringify(objectLiteral)
+);
+
+var jsonValue = JSON.parse(
+    '{ "firstname": "Mary","isAProgrammer": true }'
+);
+
+console.log(jsonValue);
