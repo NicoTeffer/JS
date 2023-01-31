70a19f2d107de41945c30019bdfdccb9e998407f
Nico Teffer <n.teffer@gmail.com>
2023-01-31 13:33:10 +0100

    call(),apply(),bind() - training

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,19 +1,41 @@
-function tellMeWhenDone(callback) {
-  var a = 1000; // some work
-  var b = 2000; // some work
+var person = {
+  firstname: "john",
+  lastname: "doe",
+  getFullName: function () {
+    var fullName = this.firstname + " " + this.lastname;
+    return fullName;
+  },
+};
 
-  callback(); // the "callback", it runs the function i gave it
+var logName = function (lang1, lang2) {
+  console.log("logged: " + this.getFullName());
+  console.log("Arguments: " + lang1 + " " + lang2);
+  console.log("------------------");
+};
+
+var logPersonName = logName.bind(person);
+
+logPersonName("en", "es");
+
+logName.call(person, "en", "es"); // invokes the function with certain parameters
+
+logName.apply(person, ["en", "es"]); // takes an array of parameters
+
+// function borrowing
+var person2 = {
+  firstname: "jane",
+  lastname: "doe",
+};
+
+console.log(person.getFullName.apply(person2));
+
+// function currying
+function multiply(a, b) {
+  return a * b;
 }
 
-tellMeWhenDone(
-  function () {
-    console.log("i am done!");
-  }
-);
+var multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));
 
-tellMeWhenDone(
-  function () {
-    alert("done!");
-  }
-);
-
+var multiplyByFive = multiply.bind(this, 5);
+console.log(multiplyByFive(2));
