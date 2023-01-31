8b8ef92ee90a715ada40fe438c5505fd28137e3d
Nico Teffer <n.teffer@gmail.com>
2023-01-31 12:34:30 +0100

    Function factories training

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,33 +1,17 @@
+function makeGreeting(language) {
+  return function (firstname, lastname) {
+    if (language === "en") {
+      console.log("Hello " + firstname + " " + lastname);
+    }
 
-// function statement
-function greet(name) {
-    console.log("Hello" + name);
+    if (language === "es") {
+      console.log("Hola " + firstname + " " + lastname);
+    }
+  };
 }
 
-greet("John");
+var greetEnglish = makeGreeting("en");
+var greetSpanish = makeGreeting("es");
 
-
-// using a function expression
-var greetFunction = function (name) {
-    console.log("Hello" + name);
-};
-
-greetFunction("John");
-
-
-// using an Immediately Invoked Function Expression (IIFE)
-var greeting = function (name) {
-    console.log("Hello" + name);
-}("John");
-
-console.log(greeting);
-
-// common usage of an IIFE
-
-var firstname = "John";
-
-(function (name) {
-
-    var greeting = "Inside IIFE: Hello";
-    console.log(greeting + " " + name);
-}(firstname)); // IIFE
+greetEnglish("Nico","Teffer");
+greetSpanish("Nicola","Miller");
