0e970506f2e5da97b15fb5dfa6e4efb421113fbc
Nico Teffer <n.teffer@gmail.com>
2023-02-01 06:37:29 +0100

    Prototype Lecture

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,42 +1,27 @@
-// functional programming notes and lecture
+// Prototype Lecture
 
-function mapForEach(arr, fn) {
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    newArr.push(fn(arr[i]));
-  }
-
-  return newArr;
-}
-
-var arr1 = [1, 2, 3];
-console.log(arr1);
-
-var arr2 = mapForEach(arr1, function (item) {
-  return item * 2;
-});
-console.log(arr2);
-
-var arr3 = mapForEach(arr1, function (item) {
-  return item > 2;
-});
-console.log(arr3);
-
-var checkPastLimit = function (limiter, item) {
-  return item > limiter;
-}.bind(this, 1);
-
-var arr4 = mapForEach(arr1, checkPastLimit);
-console.log(arr4);
-
-var checkPastLimitSimplified = function (limiter) {
-  return function (limiter, item) {
-    return item > limiter;
-  }.bind(this, limiter);
+var person = {
+  firstname: "Default",
+  lastname: "Default",
+  getFullName: function () {
+    return this.firstname + " " + this.lastname;
+  },
 };
 
-var arr5 = mapForEach(arr1, checkPastLimitSimplified(1));
-console.log(arr5);
+var john = {
+  firstname: "John",
+  lastname: "Doe",
+};
 
-// NOTE: Functional programming should not mutate data, 
-// or just return a function and hand the method over
+// DON'T DO THIS!
+
+john.__proto__ = person; // John inherit's from person
+console.log(john.getFullName());
+console.log(john.firstname);
+
+var jane = {
+  firstname: "Jane",
+};
+
+jane.__proto__ = person; // Jane inherit's from person's Default lastname since it doesn't have a lastname
+console.log(jane.getFullName());
