66abf8654ae23aa6f15c197d8cd39c055a76e935
Nico Teffer <n.teffer@gmail.com>
2023-02-02 13:01:13 +0100

    // ES6 and Classes

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,37 +1,28 @@
-// Object.create and Pure Prototypal Inheritance
-var person = {
-    firstName: "Default",
-    lastName: "Default",
-    greet: function () {
+// ES6 and Classes
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    greet() {
         return "Hi " + this.firstName;
     }
 }
-// Object.create creates an empty object with its prototype pointing at whatever u passed into Object.create!
-var john = Object.create(person);
-console.log(john); // Empty Object with "person" as its prototype
-console.log(john.greet()); // Same empty object with its prototype function inherited from person!
 
-// Overriding the "default" values
-john.firstName = "John";
-john.lastName = "Doe";
-console.log(john); // now its works with the updated values given by the prototype
-console.log(john.greet()); // same with the function inherited by the prototype
+var john = new Person("John", "Doe");
+console.log(john);
 
-// Polyfill (Legacy feature where .create doesn't work!)
-function polyfill() {
+class InformalPerson extends Person {
+    constructor(firstName, lastName) {
+        super(firstName, lastName);
+    }
 
-    if (!Object.create) {
-        Object.create = function (o) {
-            if (arguments.length > 1) {
-                throw new Error("Object.create implementation"
-                    + " only accepts the first parameter.");
-            }
-
-            function F() {
-            }
-
-            F.prototype = o;
-            return new F();
-        };
+    greet() {
+        return "Yo " + this.firstName;
     }
 }
+
+var informalJohn = new InformalPerson("John", "Doe");
+console.log(informalJohn);
