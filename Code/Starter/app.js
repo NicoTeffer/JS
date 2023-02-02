0bb2ac27c96c0e560478244cc4ffe4a60d3bec6f
Nico Teffer <n.teffer@gmail.com>
2023-02-02 16:46:15 +0100

    // Strict Mode - Lecture

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,28 +1,18 @@
-// 'typeof' , 'instanceof', and Figuring Out What Something Is
+// Strict Mode - Lecture
 
-var a = 3;
-console.log(typeof a);
+"use strict";
 
-var b = "Hello";
-console.log(typeof b);
+function logNewPerson (){
+    "use strict";
 
-var c = {};
-console.log(typeof c);
-
-var d = [];
-console.log(typeof d); // weird!
-console.log(Object.prototype.toString.call(d)); // better!
-
-function Person(name) {
-    this.name = name;
+    var person2;
+    persom2 ={};
+    console.log(persom2);
 }
 
-var e = new Person("Jane");
-console.log(typeof e);
-console.log(e instanceof Person);
+var person;
 
-console.log(typeof undefined); // makes sense
-console.log(typeof null); // a bug since, like forever... Null here is also an Object!! O.o
-
-var z = function (){};
-console.log(typeof z); // returns the typeof function! 1st class functions make it possible that u can see the type here!
+persom = {};
+console.log(persom);
+console.log(person);
+logNewPerson();
