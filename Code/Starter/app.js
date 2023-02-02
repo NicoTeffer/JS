1766e966d139f4a978617be6bc55bba3b70a1d92
Nico Teffer <n.teffer@gmail.com>
2023-02-02 12:48:33 +0100

    // Object.create and Pure Prototypal Inheritance

--- Code/Starter/app.js
+++ Code/Starter/app.js
@@ -1,12 +1,37 @@
-// !! Dangerous Aside !! Arrays and for..in
-Array.prototype.myCustomFeature = "cool!";
-
-var array = ["John", "Jane", "Jim"]; // As an array is an Object every member of the array has its position as its property name
-
-for (var prop in array) {   // That's the reason why u can use [0] for accessing a property for example
-    console.log(prop + ": " + array[prop]);
+// Object.create and Pure Prototypal Inheritance
+var person = {
+    firstName: "Default",
+    lastName: "Default",
+    greet: function () {
+        return "Hi " + this.firstName;
+    }
 }
+// Object.create creates an empty object with its prototype pointing at whatever u passed into Object.create!
+var john = Object.create(person);
+console.log(john); // Empty Object with "person" as its prototype
+console.log(john.greet()); // Same empty object with its prototype function inherited from person!
 
-// But the fact that an array is an object means that there is a problem when u add via prototype as we see in the above log
+// Overriding the "default" values
+john.firstName = "John";
+john.lastName = "Doe";
+console.log(john); // now its works with the updated values given by the prototype
+console.log(john.greet()); // same with the function inherited by the prototype
 
-// Use instead for-i loops to solve this
+// Polyfill (Legacy feature where .create doesn't work!)
+function polyfill() {
+
+    if (!Object.create) {
+        Object.create = function (o) {
+            if (arguments.length > 1) {
+                throw new Error("Object.create implementation"
+                    + " only accepts the first parameter.");
+            }
+
+            function F() {
+            }
+
+            F.prototype = o;
+            return new F();
+        };
+    }
+}
