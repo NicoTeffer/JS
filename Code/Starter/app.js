c29df44c4a91ceb37ccc190bb7cbb06e66fe5dac
Nico Teffer <n.teffer@gmail.com>
2023-02-06 11:54:18 +0100

    Updated the Repo with another Course-project

--- Code/Starter/app.js
+++
@@ -1,5 +0,0 @@
-
-// Method chaining possible due to returning this; in the functions
-var q = $("ul.people li").addClass("newClass").removeClass("people");
-console.log(q);
-
---
+++ JS-Understanding_the_weird_parts/Code/app.js
@@ -0,0 +1,23 @@
+/* Building a Framework - "GREETR"
+
+Requirements:
+
+When given a first name, last name, and optional language, it generates formal and informal
+greetings.
+
+Support for English and Spanish languages.
+
+Reusable library / framework.
+
+Easy to type 'G$()' structure.
+
+Needs to Support jQuery.
+
+*/
+
+var a = G$("John", "Doe");
+console.log(a);
+
+var b = G$("Nico", "Teffer", "de");
+console.log(b);
+
