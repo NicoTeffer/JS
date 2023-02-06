58e1be40b661d70a96d1df2d2174f2335851d5a1
Nico Teffer <n.teffer@gmail.com>
2023-02-06 21:00:20 +0100

    Assignments & coding challenges commit #2

--- JS-from_0_to_Hero/01-Fundamentals-Part-1/starter/coding_challenges.js
+++ JS-from_0_to_Hero/01-Fundamentals-Part-1/starter/coding_challenges.js
@@ -62,5 +62,40 @@ console.log(consoleMessage(participant1, testData_version2, fn_version2, bmiCalc
 
 let bmiCalculation_John_Data1_v1 = bmiCalc_v1(johnMass1, johnHeight1);
 console.log(consoleMessage(participant2, testData_version1, fn_version1, bmiCalculation_John_Data1_v1));
-let bmiCalculation_John_Data1_v2 = bmiCalc_v2(johnMass2, johnHeight2);
-console.log(consoleMessage(participant2, testData_version2, fn_version2, bmiCalculation_John_Data1_v2));
+let bmiCalculation_John_Data2_v2 = bmiCalc_v2(johnMass2, johnHeight2);
+console.log(consoleMessage(participant2, testData_version2, fn_version2, bmiCalculation_John_Data2_v2));
+
+// Comparison output
+let markHigherBMI_Data_v1 = bmiCalculation_Mark_Data1_v1 > bmiCalculation_John_Data1_v1;
+console.log("markHigherBMI Data_v1= " + markHigherBMI_Data_v1);
+let markHigherBMI_Data_v2 = bmiCalculation2_Mark_Data2_v2 > bmiCalculation_John_Data2_v2;
+console.log("markHigherBMI Data_v2= " + markHigherBMI_Data_v2);
+console.log("--------- End of Coding challenge #1 ---------");
+
+/*
+Coding Challenge #2
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+
+Your tasks:
+    1. Print a nice output to the console, saying who has the higher BMI.
+    The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+    2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+Hint: Use an if/else statement
+*/
+
+// Coding Challenge #2 - My Solution
+
+function compareBmiConditionals(bmi_data, calc_data_Mark, calc_data_John) {
+    if (bmi_data) {
+        console.log(`${participant1}'s BMI (${calc_data_Mark}) is higher than ${participant2}'s (${calc_data_John})!`);
+    } else {
+        console.log(`${participant2}'s BMI (${calc_data_John}) is higher than ${participant1}'s (${calc_data_Mark})!`);
+    }
+}
+
+compareBmiConditionals(markHigherBMI_Data_v1, bmiCalculation_Mark_Data1_v1, bmiCalculation_John_Data1_v1);
+compareBmiConditionals(markHigherBMI_Data_v2, bmiCalculation2_Mark_Data2_v2, bmiCalculation_John_Data2_v2);
+console.log("--------- End of Coding challenge #2 ---------");
