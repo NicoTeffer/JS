8392a3a720d6797c62b0d80da265ff5ba16b1c34
Nico Teffer <n.teffer@gmail.com>
2023-02-09 19:59:28 +0100

    Assignments Fundamentals Part 2 complete

--- JS-from_0_to_Hero/02-Fundamentals-Part-2/starter/assignments.js
+++ JS-from_0_to_Hero/02-Fundamentals-Part-2/starter/assignments.js
@@ -182,6 +182,57 @@ for (let i = 0; i <= 50; i++) {
     console.log(`Voter number ${i} is currently voting.`);
 }
 
+/*
+LECTURE: Looping Arrays, Breaking and Continuing
+        1. Let's bring back the 'populations' array from a previous assignment
+        2. Use a for loop to compute an array called 'percentages2' containing the percentages of the world population for the 4 population values.
+           Use the function 'percentageOfWorld1' that you created earlier
+        3. Confirm that 'percentages2' contains exactly the same values as the 'percentages' array that we created manually in the previous assignment,
+           and reflect on how much better this solution is!
+ */
 
+const percentages2 = [];
+
+for (let i = 0; i < populations.length; i++) {
+    percentages2.push(percentageOfWorld1(populations[i]));
+}
+
+console.log(percentages2);
+console.log(percentages);
+
+/*
+LECTURE: Looping Backwards and Loops in Loops
+        1. Store this array of arrays into a variable called 'listOfNeighbours'
+           [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+        2. Log only the neighbouring countries to the console, one by one, not the entire arrays.
+           Log a string like 'Neighbour: Canada' for each country.
+        3. You will need a loop inside a loop for this. This is actually a bit tricky, so don't worry if it's too difficult for you!
+           But you can still try to figure this out anyway.
+*/
+
+const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    for (let j = 0; j < listOfNeighbours[i].length; j++) {
+        console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+    }
+}
+
+/*
+LECTURE: The while Loop
+        1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing', but this time using a while loop (call the array 'percentages3')
+        2. Reflect on what solution you like better for this task: the for loop or the while loop?
+ */
+
+const percentages3 = [];
+let x = 0;
+while (x < populations.length) {
+    percentages3.push(percentageOfWorld1(populations[x]));
+    x++;
+}
+
+console.log(percentages);
+console.log(percentages2);
+console.log(percentages3);
 
 console.log("--------- End of Assignments File ---------");
